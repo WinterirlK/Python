214ed6678a3cf79124efbeaafc628b170a68243b
WinterirlK <138369381+WinterirlK@users.noreply.github.com>
2023-11-18 17:14:15 +0100

    Create Klasy.py

---
+++ Klasy.py
@@ -0,0 +1,15 @@
+
+class witam:
+    
+    def silnia(A):
+        if A>2:
+            return 1
+        else:
+            return A * silnia(A-1)
+
+
+    def fib(n):
+   if n <= 1:
+       return n
+   else:
+       return(fib(n-1) 
